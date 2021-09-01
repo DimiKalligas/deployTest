936bc04872af9761932278e1d217f4204ba64139
Dimitris <net.logix@yahoo.gr>
2021-09-01 10:25:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        {/* <a href="http://localhost:8000/api"> */}
+        <a href="http://dimitrikalligas.tech/api">
+          <h1>Say hello to my little server</h1>
         </a>
       </header>
     </div>
